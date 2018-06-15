423fbaca9a6f508548c282f0d0b53e8cfec142da
Nanda Arfan Hakim <25836292+sonadztux@users.noreply.github.com>
2018-06-15 10:01:39 +0700

    Add three .js files
    All .js files stored in this folder.

---
+++ js/animate.js
@@ -0,0 +1,114 @@
+function animate(typewriter) {
+    sequence = [
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Assalamu ‘alaikum wa rahmatullahi wa barakatuh,\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Saya Nanda Arfan Hakim mengucapkan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Selamat Hari Raya Idul Fitri 1439 Hijriah.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Serta di hari kemenangan, hari yang suci dan penuh berkah ini,\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "jika saya pernah berbuat salah baik dari perilaku maupun...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "perkataan kepada kalian, maka ingatlah bahwa itu semua...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "MEMANG PANTAS UNTUK KALIAN BANGS\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\badalah benar-benar kekhilafan saya.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Oleh karena itu, saya atas nama pribadi juga ingin meminta...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "maaf sebesar-besarnya untuk kekhilafan-kekhilafan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "yang pernah dilakukan.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Minal Aidzin Wal Faidzin.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@nanda:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Wassalamu 'alaikum wa rahmatullahi wa barakatuh.\n",
+            delayAfter: 500
+        },
+    ];
+
+    typewriter.playSequence(sequence);
+}
