6cb50e8750048597934596516e24dc4112d26555
Nanda Arfan Hakim <dev.sonadztux@gmail.com>
2022-05-01 21:40:50 +0700

    :)

--- js/animate.js
+++ js/animate.js
@@ -3,65 +3,65 @@ function animate(typewriter) {
         {
             text: "root@nanda:~# ",
             instant: true,
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "Assalamu'alaikum Warrahmatullahi Wabarakatuh,\n",
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "root@nanda:~# ",
             instant: true,
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "Selamat Hari Raya Idul Fitri 1439 Hijriah, saudaraku.\n",
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "root@nanda:~# ",
             instant: true,
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "Saya Nanda, di hari yang suci dan penuh berkah ini, apabila saya pernah berbuat salah baik dari perilaku maupun lisan\n",
-            delayAfter: 500
-        }
+            delayAfter: 350
+        },
         {
             text: "root@nanda:~# ",
             instant: true,
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "maka ingatlah bahwa itu semua... MEMANGLAH SAYA SENGAJA\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\badalah benar-benar kekhilafan saya.\n",
-            delayAfter: 500
+            delayAfter: 350
         }
         {
             text: "root@nanda:~# ",
             instant: true,
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "Oleh karena itu, saya atas nama pribadi ingin meminta maaf sebesar-besarnya atas segala kekhilafan saya.\n",
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "root@nanda:~# ",
             instant: true,
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "Minal Aidzin Wal Faidzin.\n",
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "root@nanda:~# ",
             instant: true,
-            delayAfter: 500
+            delayAfter: 350
         },
         {
             text: "Wassalamu'alaikum Warrahmatullahi Wabarakatuh.\n",
-            delayAfter: 500
+            delayAfter: 350
         },
     ];
 
